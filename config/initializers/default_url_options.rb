2066375ceb40e169cd20c599fd212a4c1c2249ac
DesenvolvedorValdar <168828822+DesenvolvedorValdar@users.noreply.github.com>
2025-05-02 08:51:46 -0300

    Update default_url_options.rb url

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,1 +1,11 @@
+# Força as opções corretas de URL mesmo fora de contexto de request
+Rails.application.routes.default_url_options = {
+  host: ENV.fetch('RAILS_HOST', 'localhost'),
+  protocol: ENV.fetch('RAILS_PROTOCOL', 'https')
+}
+
+ActiveStorage::Current.url_options = {
+  host: ENV.fetch('RAILS_HOST', 'localhost'),
+  protocol: ENV.fetch('RAILS_PROTOCOL', 'https')
+}
 
